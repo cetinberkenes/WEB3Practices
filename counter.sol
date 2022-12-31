25c5c0def8c6fa9f04fcfe72e4a7bf7c94bff32b
cetinberkenes <69789224+cetinberkenes@users.noreply.github.com>
2022-12-31 15:44:50 +0300

    Add files via upload

---
+++ counter.sol
@@ -0,0 +1,16 @@
+
+pragma solidity ^0.8.7;
+
+contract counter {
+    uint public count;
+
+    function inc() external {
+        count +=1;
+
+    }
+    function dec() external{
+        count -=1;
+
+    }
+
+}
